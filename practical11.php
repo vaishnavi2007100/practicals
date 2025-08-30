7179b9c9c8da5443f1ceb9401a69ba2ddd208a75
vaishnavi2007100 <yadavvaishnavi292@gmail.com>
2025-08-30 15:42:10 +0530

    Add files via upload

---
+++ practical11.php
@@ -0,0 +1,7 @@
+<?php
+    // Display heading
+    print "<h1>Welcome to My Webpage</h1>";
+
+    // Display paragraph
+    print "<p>This is a simple paragraph displayed using PHP print statement.</p>";
+?>
