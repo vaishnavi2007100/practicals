cc855045ad162b9314ca60719ce7354b21a35653
vaishnavi2007100 <yadavvaishnavi292@gmail.com>
2025-08-30 15:42:34 +0530

    Add files via upload

---
+++ practical12.php
@@ -0,0 +1,8 @@
+<?php
+    $num1 = 10;
+    $num2 = 20;
+
+    echo "<h3>First Number: $num1</h3>";
+    echo "<h3>Second Number: $num2</h3>";
+    echo "<h3>Sum = " . ($num1 + $num2) . "</h3>";
+?>
